a08fec8c2bf75e3f2e6ad3e50b83fed6238ed258
Arham Khan <148587739+WildSparrow45@users.noreply.github.com>
2023-10-21 02:26:51 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+// import bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+// component import
+
+import Routepage from "./Routing/Routepage";
+
+function App() {
+  return (
+    <div className="App">
+      <Routepage />
+    </div>
+  );
+}
+
+export default App;
